2d7b6bc50a2b18c81d983bd70ec0daf9d30fa5ba
AidanUnhappy <aidanczhai@gmail.com>
2022-04-21 14:59:22 +0800

    add solarized colors

--- config.h
+++ config.h
@@ -16,11 +16,29 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_suckless[]    = "#005577";
+
+static const char col_base03[]      = "#002b36";
+static const char col_base02[]      = "#073642";
+static const char col_base01[]      = "#586e75";
+static const char col_base00[]      = "#657b83";
+static const char col_base0[]       = "#839496";
+static const char col_base1[]       = "#93a1a1";
+static const char col_base2[]       = "#eee8d5";
+static const char col_base3[]       = "#fdf6e3";
+static const char col_yellow[]      = "#b58900";
+static const char col_orange[]      = "#cb4b16";
+static const char col_red[]         = "#dc322f";
+static const char col_magenta[]     = "#d33682";
+static const char col_violet[]      = "#6c71c4";
+static const char col_blue[]        = "#268bd2";
+static const char col_cyan[]        = "#2aa198";
+static const char col_green[]       = "#859900";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg                    bg                    border       */
+	[SchemeNorm] = { col_gray3,            col_gray1,            col_gray2     },
+	[SchemeSel]  = { col_gray4,            col_suckless,         col_suckless  },
 };
 
 /* tagging */
@@ -62,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_suckless, "-sf", col_gray4, NULL };
 static const char *termcmd[]     = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 
